4592fde77403cc55da904b1f2b1703c85d79c85d
Tristan Cholette <tristancholette@gmail.com>
2024-11-23 23:27:36 -0500

    microphone and button game

---
+++ Assets/Tristan Things/LaunchPadButton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum LaunchColor {
+    Red,
+    Green,
+    Blue,
+    Null
+}
+
+public class LaunchPadButton : MonoBehaviour
+{
+
+
+    public void Start() {
+        if (color == LaunchColor.Green) {
+            manager.score++;
+        }
+    }
+
+    public LaunchColor color;
+    public LaunchpadGame manager;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log(manager.score);
+        manager.ChangeColor(gameObject);
+    }
+    void OnMouseEnter() {
+        Debug.Log(manager.score);
+        manager.ChangeColor(gameObject);
+    }
+}
